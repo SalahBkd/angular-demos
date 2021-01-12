d3c093201e8e401b1241b959666295a6e74e9aaf
Salah Eddine Boukadi <bkdsalah@gmail.com>
2021-01-12 15:45:28 +0100

    add built in & custom validation

--- src/app/persons/person-form/person-form.component.ts
+++ src/app/persons/person-form/person-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-person-form',
@@ -11,11 +11,34 @@ export class PersonFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      name: new FormControl(''),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
       email: new FormControl(''),
-      gender: new FormControl('Male')
+      gender: new FormControl('Male'),
+      age: new FormControl('', this.ageValidator),
     });
 
+  }
+
+  ageValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const age = parseInt(control.value, 10);
+    const minAge = 15;
+    const maxYear = 100;
+    if (age >= minAge && age <= maxYear) {
+      return null;
+    } else {
+      return {
+        year: {
+          min: minAge,
+          max: maxYear
+        }
+      };
+    }
   }
 
   onSubmit(person) {
